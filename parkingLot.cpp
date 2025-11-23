474194cd852a0d86f2586cf5ef0c3c89f6f718e4
Anish <36012314+AKA333@users.noreply.github.com>
2025-11-23 14:28:37 +0000

    added parking class

--- parkingLot.cpp
+++ parkingLot.cpp
@@ -34,8 +34,12 @@ public:
 		return true;
 	}
 	Vehicle unparkVehicle(){
-		Vehicle* temp = vehicle;
-		
+		if(isAvailable)
+			return nullptr;
+		Vehicle* vehicle = parkedVehicle;
+		parkedVehicle = nullptr;
+		isAvailable= true;
+		return vehicle;
 	}
 
 private:
@@ -59,18 +63,71 @@ private:
 class Floor{
 	int small, mid, large;
 	int floorId;
+	vector<ParkingSpot> spots;
 public:
-	Floor(id, small, mid, large){floorId: id, small: small, mid: mid, large:large}
-	int getFloorId(){
-		return floorId;
+	Floor(int floorId, int small, int mid, int large): floorId(floorId), 
+	small(small), ,mid(mid), large(large){
+		int total= small+mid+large;
+		int i=0;
+		for(int i=0; i<total; i++){
+			VehicleType type;
+			switch(i){
+			case i<small:
+				type = VehicleType::bike;
+			case i<mid and i>=small:
+				type = VehicleType::car
+			case i<large && i>=mid:
+				type = VehicleType::truck;
+			default:
+				type= VehicleType::car;
+			}
+
+			ParkingSpot spot(type, genId(i));
+			spots.push_back(spot);
+		}
+	}
+
+	string genId(int i){
+		return "f"+to_string(floorId)+to_string(i);
+	}
+
+	ParkingSpot isSpotAvailable(Vehicle vehicle){
+		VehicleType type = vehicle.type;
+		for(auto spot: spots){
+			if(spot.type == type && spot.isAvailable()){
+				return spot;
+			}
+		}
+		return nullptr;
 	}
 };
 
 class ParkingLot{
 	vector<Floor>parking;
 public:
-	bool isParkingSpotAvailable(Vehicle veh){
+	void addFloor(Floor floor){
+		parking.push_back(floor)
+	}
+
+	bool parkVehicle(Vehicle* vehicle){
+		if(!vehicle)
+			return false;
+		for(auto floor:parking){
+			ParkingSpot vacantSpot= floor.isSpotAvailable(vehicle);
+			if(vacantSpot){
+				return vacantSpot.parkVehicle(vehicle);
+			}
+		}
+		cout<<"no spot isAvailable\n";
+		return false;
 
 	}
 
+	unparkVehicle(Vehicle vehicle){
+
+	}
+
+	void displayStatus(){
+
+	}
 };
